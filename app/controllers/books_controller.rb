e1f90b1274b56208de740d6be4d64d3f23624463
kensei1016 <kensei1016ab@gmail.com>
2020-02-23 16:04:07 +0900

    バグ修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,14 @@ class BooksController < ApplicationController
   end
 
   def index
-  	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+		@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+		@book = Book.new # 新規登録フォーム用
   end
 
-  def create
-  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+	def create
+		# @book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+		# @book.user_id = current_user.id
+		@book = current_user.books.build(book_params)
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
@@ -33,16 +36,16 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
