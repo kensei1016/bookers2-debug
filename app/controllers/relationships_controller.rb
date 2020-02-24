e170201f39822a5b873fcd74f7a5bd74926f185e
kensei1016 <kensei1016ab@gmail.com>
2020-02-24 21:36:25 +0900

    user profileにフォローボタン追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,16 @@
 class RelationshipsController < ApplicationController
   def create
+    follow_user = User.find(params[:user_id])
+    relationship = Relationship.new(user_id: current_user.id, follow_user_id: follow_user.id)
+    if relationship.save
+      redirect_to user_path(follow_user)
+    end
   end
 
   def destroy
+    follow_user = User.find(params[:user_id])
+    relationship = Relationship.find_by(user_id: current_user.id, follow_user_id: follow_user.id)
+    relationship.destroy
+    redirect_to user_path(follow_user)
   end
 end
