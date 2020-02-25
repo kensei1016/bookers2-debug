9ab924a902077b993f474f226aaf835fd8c12516
kensei1016 <kensei1016ab@gmail.com>
2020-02-25 20:08:50 +0900

    いいねのajax化完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,16 @@ class FavoritesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.build(book_id: @book.id)
-    if favorite.save
-      redirect_back(fallback_location: root_path)
-    end
+    favorite.save
+
+    render 'post_book'
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.find_by(user_id: current_user.id)
-    if favorite.destroy
-      redirect_back(fallback_location: root_path)
-    end
+    favorite.destroy
+
+    render 'post_book'
   end
 end
